05e998892d05e66821ce16f2dc2ca502f63bee97
Suhas Madane <madanesuhas9@gmail.com>
2023-06-14 11:49:41 +0530

    lorem

--- src/App.js
+++ src/App.js
@@ -20,7 +20,12 @@ function Header() {
 function World() {
   return (
     <>
-      <span style={{ backgroundColor: "skyblue", color: "orange" }}>Suhas</span>
+      <div className="bg-success-subtle">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        eaque praesentium odio illum, eum, nisi commodi quia cum architecto
+        molestiae repellendus similique consectetur voluptates velit? Laudantium
+        quod repudiandae tempore cumque!
+      </div>
     </>
   );
 }
