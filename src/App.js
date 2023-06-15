3c63486573d90c4f7e8c87fd29ddc1e0fd2eed64
Suhas Madane <madanesuhas9@gmail.com>
2023-06-15 07:04:58 +0530

    destructuring in array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import Footer from "./pages/footer";
-import Header from "./pages/header";
-
 function App() {
   return (
     <>
       <h1>Counter App</h1>
-      <CountLikes />
     </>
   );
 }
 
-function CountLikes() {
-  let counter = 1;
-  let likecount = () => {
-    alert();
-    console.log("I am Like button");
-  };
-  return (
-    <>
-      <h1>{counter}</h1>
-      <input type="button" value="Like" onClick={likecount} />
-    </>
-  );
-}
 export default App;
---
+++ src/pages/1.js
@@ -0,0 +1,3 @@
+let list = ["suhas","suraj","vaibhav","tejas"];
+let [,p1, ,p2] = list;
+console.log(p1,p2)
