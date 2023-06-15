d4df308ccf8ad1862ccc1ef06cd496cfcf750e0f
Suhas Madane <madanesuhas9@gmail.com>
2023-06-15 08:19:07 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
-      <h1>Counter App</h1>
+      <h3>Counter App</h3>
+      <CounterApp />
+      <CounterApp />
+    </>
+  );
+}
+
+function CounterApp() {
+  //let counter = 1;   stateless variable
+  let [counter, setCounter] = useState(1); //stateless variable converted to stateful
+  let likeAction = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    //console.log(counter);
+  };
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum iusto
+        consequuntur soluta, error, distinctio doloribus libero dignissimos
+        nisi, esse autem totam tempore dolor sapiente ullam! Placeat numquam
+        delectus sunt suscipit.
+      </p>
+      <h1>{counter}</h1>
+      <input type="button" value="Like" onClick={likeAction} />
     </>
   );
 }
