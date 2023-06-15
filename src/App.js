f6572ebe3c6a2ed8eb779f0415c51e42b27423cf
Suhas Madane <madanesuhas9@gmail.com>
2023-06-15 08:31:36 +0530

    string-updation

--- src/App.js
+++ src/App.js
@@ -5,29 +5,31 @@ function App() {
     <>
       <h3>Counter App</h3>
       <CounterApp />
-      <CounterApp />
     </>
   );
 }
 
 function CounterApp() {
   //let counter = 1;   stateless variable
-  let [counter, setCounter] = useState(1); //stateless variable converted to stateful
-  let likeAction = () => {
-    counter = counter + 1;
-    setCounter(counter);
-    //console.log(counter);
+  let [str, updateStr] = useState("HELLO WORLD"); //stateless variable converted to stateful
+  let lowerCaseAction = () => {
+    str = str.toLowerCase();
+    updateStr(str);
+  };
+  let upperCaseAction = () => {
+    str = str.toUpperCase();
+    updateStr(str);
   };
   return (
     <>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum iusto
-        consequuntur soluta, error, distinctio doloribus libero dignissimos
-        nisi, esse autem totam tempore dolor sapiente ullam! Placeat numquam
-        delectus sunt suscipit.
-      </p>
-      <h1>{counter}</h1>
-      <input type="button" value="Like" onClick={likeAction} />
+      <h1>{str}</h1>
+      <input type="button" value="LowerCase" onClick={lowerCaseAction} />
+      <input
+        type="button"
+        value="UpperCase"
+        onClick={upperCaseAction}
+        className="m-2"
+      />
     </>
   );
 }
